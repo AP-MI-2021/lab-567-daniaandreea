fdcbca7aa68ce84a34f81c6bc313218a993853b4
daniaandreea <chitandania@yahoo.com>
2021-10-27 19:19:14 +0300

    f

---
+++ main.py
@@ -0,0 +1,21 @@
+from Logic.crud import add_rezervare
+from UserInterface.console import run_console
+
+
+def main():
+    lst_rezervari = []
+    add_rezervare(lst_rezervari, '1', 'Georgescu', 'economy', 150, 'da')
+    add_rezervare(lst_rezervari, '2', 'Ionescu', 'business', 450, 'da')
+    add_rezervare(lst_rezervari, '3', 'Popescu', 'business', 555, 'da')
+    add_rezervare(lst_rezervari, '4', 'Popa', 'economy plus', 300, 'nu')
+    add_rezervare(lst_rezervari, '5', 'Barbu', 'economy', 180, 'da')
+    add_rezervare(lst_rezervari, '6', 'Ion', 'economy', 155, 'nu')
+    add_rezervare(lst_rezervari, '7', 'Marin', 'business', 480, 'nu')
+    add_rezervare(lst_rezervari, '8', 'Dobrin', 'economy plus', 333, 'da')
+    add_rezervare(lst_rezervari, '9', 'Serban', 'economy plus', 295, 'nu')
+    add_rezervare(lst_rezervari, '10', 'Ion', 'economy', 210, 'da')
+
+    x = input('Dați 1 pt. comanda de tip meniu sau 2 pt. comanda de tip line: ')
+    run_console(lst_rezervari)
+
+ main()
