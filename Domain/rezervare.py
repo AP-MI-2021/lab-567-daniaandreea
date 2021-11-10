e1f87dc6e8663655919f905c4f1e32cda6661cbd
daniaandreea <chitandania@yahoo.com>
2021-11-10 16:24:08 +0200

    dictionar

--- Domain/rezervare.py
+++ Domain/rezervare.py
@@ -1,67 +1,3 @@
-
-def get_id(rezervare):
-    '''
-    Getter pt id-ul rezervarii
-    :param rezervare: rezervarea
-    :return:id-ul rezervarii dat ca parametru
-    '''
-    return rezervare[0]
-
-def get_nume(rezervare):
-    '''
-    Getter pt numele clientului
-    :param rezervare:rezervarea
-    :return:numele rezervarii dat ca parametru
-    '''
-    return rezervare[1]
-
-def get_clasa(rezervare):
-    '''
-    Getter pt clasa rezervarii
-    :param rezervare: rezervarea
-    :return:clasa rezervarii dat ca parametru
-    '''
-    return rezervare[2]
-
-def set_clasa(rezervare, clasa):
-    """
-    Modifică clasa unei rezervări.
-    :param rezervare: rezervarea de modificat
-    :param clasa: noua clasă
-    :return:
-    """
-    rezervare[2] = clasa
-
-def get_pret(rezervare):
-    '''
-    Getter pt pretul rezervarii
-    :param rezervare: rezervarea
-    :return:pretul rezervarii dat ca parametru
-    '''
-    return rezervare[3]
-
-def set_pret(rezervare, pret):
-    """
-    Modifică prețul unei rezervări.
-    :param rezervare: rezervarea de modificat
-    :param pret: noul preț
-    :return:
-    """
-    rezervare[3] = pret
-
-
-def get_checkin(rezervare):
-    '''
-    Getter pt checkinul rezervarii
-    :param rezervare: rezervarea
-    :return:checkin-ul rezervarii dat ca parametru
-    '''
-    return rezervare[4]
-
-def get_str(rezervare):
-    return f'Rezervarea cu id-ul:{get_id(rezervare)}, numele:{get_nume(rezervare)}, la clasa :{get_clasa(rezervare)},/' \
-           f' cu pretul:{get_pret(rezervare)} si checkinul:{get_checkin(rezervare)}'
-
 def creeaza_rezervare(id_rezervare, nume, clasa, pret, checkin):
     '''
     Creeaza o rezervare la o companie aeriana.
@@ -73,10 +9,74 @@ def creeaza_rezervare(id_rezervare, nume, clasa, pret, checkin):
     :return:
     '''
 
-    obiect = []
-    obiect.append(id_rezervare)
-    obiect.append(nume)
-    obiect.append(clasa)
-    obiect.append(float(pret))
-    obiect.append(checkin)
-    return obiect
+    return {
+        'id': id_rezervare,
+        'nume':nume,
+        'clasa':clasa,
+        'pret':pret,
+        'checkin': checkin
+    }
+
+def get_id(rezervare):
+    '''
+    Getter pt id-ul rezervarii
+    :param rezervare: rezervarea
+    :return:id-ul rezervarii dat ca parametru
+    '''
+    return rezervare['id']
+
+def get_nume(rezervare):
+    '''
+    Getter pt numele clientului
+    :param rezervare:rezervarea
+    :return:numele rezervarii dat ca parametru
+    '''
+    return rezervare['nume']
+
+def get_clasa(rezervare):
+    '''
+    Getter pt clasa rezervarii
+    :param rezervare: rezervarea
+    :return:clasa rezervarii dat ca parametru
+    '''
+    return rezervare['clasa']
+
+def set_clasa(rezervare, clasa):
+    """
+    Modifică clasa unei rezervări.
+    :param rezervare: rezervarea de modificat
+    :param clasa: noua clasă
+    :return:
+    """
+    rezervare['clasa'] = clasa
+
+def get_pret(rezervare):
+    '''
+    Getter pt pretul rezervarii
+    :param rezervare: rezervarea
+    :return:pretul rezervarii dat ca parametru
+    '''
+    return rezervare['pret']
+
+def set_pret(rezervare, pret):
+    """
+    Modifică prețul unei rezervări.
+    :param rezervare: rezervarea de modificat
+    :param pret: noul preț
+    :return:
+    """
+    rezervare['pret'] = pret
+
+
+def get_checkin(rezervare):
+    '''
+    Getter pt checkinul rezervarii
+    :param rezervare: rezervarea
+    :return:checkin-ul rezervarii dat ca parametru
+    '''
+    return rezervare['checkin']
+
+def get_str(rezervare):
+    return f'Rezervarea cu id-ul:{get_id(rezervare)}, numele:{get_nume(rezervare)}, la clasa :{get_clasa(rezervare)},/' \
+           f' cu pretul:{get_pret(rezervare)} si checkinul:{get_checkin(rezervare)}'
+
